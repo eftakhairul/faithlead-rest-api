4dfb404eb0136399e115b5d6c018022d1dfce9b6
Eftakhairul Islam <eftakhairul@gmail.com>
2013-06-01 00:21:42 +0600

    company category is done

--- src/Faithlead/RestBundle/Controller/CompanyCategoryController.php
+++ src/Faithlead/RestBundle/Controller/CompanyCategoryController.php
@@ -17,10 +17,9 @@ use FOS\RestBundle\Controller\Annotations\View,
     FOS\RestBundle\Controller\Annotations\QueryParam,
     FOS\RestBundle\Controller\Annotations\RouteResource;
 
-use Faithlead\RestBundle\Document\User,
-    Faithlead\RestBundle\Document\EmailHistory,
-    Faithlead\RestBundle\Document\Tag,
-    Faithlead\RestBundle\Form\Type\EmailHistoryType;
+use Faithlead\RestBundle\Document\CompanyCategory,
+    Faithlead\RestBundle\Document\Subcategory,
+    Faithlead\RestBundle\Form\Type\CompanyCategoryType;
 
 
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
@@ -30,10 +29,10 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
  * @package Faithlead\Bundle\RestBundle\Controller
  * @RouteResource("Companycategory")
  */
-class CompanyCategory extends FosRestController
+class CompanyCategoryController extends FosRestController
 {
     /**
-     * Get total of  Email History by User Id
+     * Get total of company category
      *
      * @return array data
      *
@@ -42,17 +41,15 @@ class CompanyCategory extends FosRestController
      */
     public function getCountAction()
     {
-        if (empty($userId)) return new Response('user not found', 404);
-
-        $dm                      = $this->get('doctrine.odm.mongodb.document_manager');
-        $emailHistoryRepository = $dm->getRepository('FaithleadRestBundle:EmailHistory');
+        $dm                        = $this->get('doctrine.odm.mongodb.document_manager');
+        $compnayCategoryRepository = $dm->getRepository('FaithleadRestBundle:CompanyCategory');
 
 
-        return array('count' => $emailHistoryRepository->countByUserId($userId));
+        return array('count' => $compnayCategoryRepository->findCount());
     }
 
     /**
-     * Get the details of Email History by Id
+     * Get the details of company category by Id
      *
      * @param int $id
      * @return array data
@@ -60,70 +57,60 @@ class CompanyCategory extends FosRestController
      * @View()
      * @ApiDoc(statusCodes={200="Returned when successful",
      *                      404="Returned when id found"},
-     *         output="Faithlead\RestBundle\Document\EmailHistory"
+     *         output="Faithlead\RestBundle\Document\CompanyCategory"
      * )
      */
     public function getAction($id)
     {
-        $dm                      = $this->get('doctrine.odm.mongodb.document_manager');
-        $emailHistoryRepository  = $dm->getRepository('FaithleadRestBundle:EmailHistory');
-        $emailHistoryEntity     = $emailHistoryRepository->findOneById($id);
+        if (empty($id)) return new Response('Id not found.', 404);
 
-        if (empty($emailHistoryEntity)) return new Response('Id not found.', 404);
+        $dm                        = $this->get('doctrine.odm.mongodb.document_manager');
+        $companyCategoryRepository = $dm->getRepository('FaithleadRestBundle:CompanyCategory');
+        $companyCategoryEntity     = $companyCategoryRepository->findOneById($id);
 
-        return array('id'           => $emailHistoryEntity->getId(),
-                    'subject'       => $emailHistoryEntity->getSubject(),
-                    'user_id'       => $emailHistoryEntity->getUser()->getId(),
-                    'email_address' => $emailHistoryEntity->getEmailAddress(),
-                    'status'        => $emailHistoryEntity->getStatus(),
-                    'tags'          => $emailHistoryEntity->getTag()->getValues(),
-                    'success'       => true
-                );
+        if (empty($companyCategoryEntity)) return new Response('Id not found.', 404);
+
+        return array('id'            => $companyCategoryEntity->getId(),
+                     'name'          => $companyCategoryEntity->getName(),
+                     'subcategories' => $companyCategoryEntity->getSubcategory()->getValues(),
+                     'create_date'   => $companyCategoryEntity->getCreatedAt()
+               );
     }
 
     /**
-     * Create a new Email History by User Id
+     * Create a new Company Category
      *
-     * @param int $userId
      * @return View view instance
      *
      * @View()
-     * @ApiDoc(input="Faithlead\RestBundle\Form\Type\EmailHistoryType",
+     * @ApiDoc(input="Faithlead\RestBundle\Form\Type\CompanyCategoryType",
      *         statusCodes={200="Returned when successful",
      *                      404="Returned when user id found"},
-     *         output="Faithlead\RestBundle\Document\EmailHistory"
+     *         output="Faithlead\RestBundle\Document\CompanyCategory"
      * )
      */
-    public function postAction($userId)
+    public function postAction()
     {
-        if (empty($userId)) return new Response('user not found', 404);
-
-        $dm                  = $this->get('doctrine.odm.mongodb.document_manager');
-        $emailHistoryEntity = new EmailHistory();
-        $form                = $this->getForm($emailHistoryEntity);
-        $request             = $this->getRequest();
+        $dm                      = $this->get('doctrine.odm.mongodb.document_manager');
+        $companyCategoryEntity   = new CompanyCategory();
+        $form                    = $this->getForm($companyCategoryEntity);
+        $request                 = $this->getRequest();
 
         if ('POST' == $request->getMethod()) {
 
             $form->bind(array(
-                "status"          => $request->request->get('body'),
-                "subject"         => $request->request->get('subject'),
-                "email_address"   => $request->request->get('email_address'),
+                "name"         => $request->request->get('subject')
                 )
             );
 
             if ($form->isValid()) {
-                $userRepository = $dm->getRepository('FaithleadRestBundle:User');
-                $userEntity     = $userRepository->findOneById($userId);
 
-                $tags = explode(',', $request->request->get('tags', array()));
-                foreach($tags as $key => $tag) $emailHistoryEntity->setOneTag(new Tag($tag));
-
-                $emailHistoryEntity->setUser($userEntity);
-                $dm->persist($emailHistoryEntity);
+                $subcategories = explode(',', $request->request->get('subcategories', array()));
+                foreach($subcategories as $key => $subcategory) $companyCategoryEntity->setOneSubcategory(new Subcategory($subcategory));
+                $dm->persist($companyCategoryEntity);
                 $dm->flush();
 
-                return array('id' => $emailHistoryEntity->getId());
+                return array('id' => $companyCategoryEntity->getId());
             } else {
                 return array($form);
             }
@@ -131,7 +118,7 @@ class CompanyCategory extends FosRestController
     }
 
     /**
-     * Delete the specific Email history by Id
+     * Delete the specific company category by Id
      *
      * @param int $id
      * @return array data
@@ -143,20 +130,22 @@ class CompanyCategory extends FosRestController
      */
     public function deleteAction($id)
     {
-        $dm                      = $this->get('doctrine.odm.mongodb.document_manager');
-        $emailHistoryRepository  = $dm->getRepository('FaithleadRestBundle:EmailHistory');
-        $emailHistoryEntity      = $emailHistoryRepository->findOneById($id);
+        if (empty($id)) return new Response('Id not found.', 404);
 
-        if (empty($emailHistoryEntity)) return new Response('Id not found.', 404);
+        $dm                        = $this->get('doctrine.odm.mongodb.document_manager');
+        $companyCategoryRepository = $dm->getRepository('FaithleadRestBundle:CompanyCategory');
+        $companyCategoryEntity     = $companyCategoryRepository->findOneById($id);
 
-        $dm->remove($emailHistoryEntity);
+        if (empty($companyCategoryEntity)) return new Response('Id not found.', 404);
+
+        $dm->remove($companyCategoryEntity);
         $dm->flush();
 
         return new Response('success', 200);
     }
 
     /**
-     * Update an Email History by Id
+     * Update an company category by Id
      *
      * @param int $id
      * @return View view instance
@@ -168,33 +157,33 @@ class CompanyCategory extends FosRestController
     public function putAction($id)
     {
 
-        $dm                      = $this->get('doctrine.odm.mongodb.document_manager');
-        $emailHistoryRepository  = $dm->getRepository('FaithleadRestBundle:EmailHistory');
-        $emailHistoryEntity      = $emailHistoryRepository->findOneById($id);
+        if (empty($id)) return new Response('Id not found.', 404);
 
-        if (empty($emailHistoryEntity)) return new Response('Id not found.', 404);
+        $dm                        = $this->get('doctrine.odm.mongodb.document_manager');
+        $companyCategoryRepository = $dm->getRepository('FaithleadRestBundle:CompanyCategory');
+        $companyCategoryEntity     = $companyCategoryRepository->findOneById($id);
 
-        $emailHistoryEntity->setSubject($this->getRequest()->request->get('subject'));
-        $emailHistoryEntity->setStatus($this->getRequest()->request->get('status'));
-        $emailHistoryEntity->setEmailAddress($this->getRequest()->request->get('email_address'));
+        if (empty($companyCategoryEntity)) return new Response('Id not found.', 404);
 
-        $tags = explode(',', $this->getRequest()->request->get('tags', array()));
-        foreach($tags as $key => $tag) $emailHistoryEntity->setOneTag(new Tag($tag));
+        $companyCategoryEntity->setName($this->getRequest()->request->get('name'));
 
-        $dm->persist($emailHistoryEntity);
+        $subcategories = explode(',', $this->getRequest()->request->get('subcategories', array()));
+        foreach($subcategories as $key => $subcategory) $companyCategoryEntity->setOneSubcategory(new Subcategory($subcategory));
+
+        $dm->persist($companyCategoryEntity);
         $dm->flush();
 
         return new Response('success', 200);
     }
 
     /**
-     * Return a email history form
+     * Return a company category form
      *
-     * @param null $emailHistoryEntity
+     * @param null $companyCategoryEntity
      * @return \Symfony\Component\Form\Form
      */
-    protected function getForm($emailHistoryEntity = null)
+    protected function getForm($companyCategoryEntity = null)
     {
-        return $this->createForm(new EmailHistoryType(), $emailHistoryEntity);
+        return $this->createForm(new CompanyCategoryType(), $companyCategoryEntity);
     }
 }
---
+++ src/Faithlead/RestBundle/Form/Type/CompanyCategoryType.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Company Category
+ *
+ * @author Eftakahirul Islam  <eftakhairul@gmail.com>
+ * Copyright @ Faithlead
+ */
+namespace Faithlead\RestBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CompanyCategoryType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text');
+        $builder->add('subcategories', 'text');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'        => 'Faithlead\RestBundle\Document\CompanyCategory',
+            'csrf_protection'   => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'compnaycategory';
+    }
+}
